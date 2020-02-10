09c4c9853a807ce3ce7b5f5f366684fa48b0c78e
Noriega <cesar.noriega@capgemini.com>
2020-02-09 20:35:12 -0600

    Project deleted and rebuilt with new name

---
+++ src/main/java/com/springboot/restclient/actuator/SpringbootRestApiWithActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.restclient.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestApiWithActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestApiWithActuatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/restclient/actuator/SpringbootRestApiWithActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.restclient.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApiWithActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
